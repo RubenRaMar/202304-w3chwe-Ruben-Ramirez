ffb411f13517835472ee9a7a6de21371f1c9afe0
RubenRaMar <rubenfr1@hotmail.com>
2023-04-23 13:31:58 +0200

    Add the creation of the ArrowButtonComponent component in AppComponent

--- src/ts/components/AppComponent/AppComponent.ts
+++ src/ts/components/AppComponent/AppComponent.ts
@@ -1,3 +1,4 @@
+import ArrowButtonComponent from "../ArrowButtonComponent/ArrowButtonComponent.js";
 import Component from "../Component/Component.js";
 
 class AppComponent extends Component {
@@ -20,13 +21,27 @@ class AppComponent extends Component {
       <h2 class="pokedex__title">Pokédex</h2>
       <p class="pokedex__info">Choose your favorite Pokémon</p>
       <ul class="arrows-contanier">
-        <li><button class="arrows-contanier__left">izquierda</button>
-        </li>
+        <li class="arrow-left"></li>
         <li class="arrows-contanier__displayed-cards">24/500</li>
-        <li><button class="arrows-contanier__right">derecha</button></li>
-      /ul>
+        <li class="arrow-right"></li>
+      </ul>
     </section>
     `;
+
+    const arrowLeft = this.element.querySelector(".arrow-left")!;
+    const arrowRight = this.element.querySelector(".arrow-right")!;
+
+    new ArrowButtonComponent(
+      arrowLeft,
+      `<i class="fa-solid fa-left-long"></i>`,
+      "arrows-contanier__left button"
+    );
+
+    new ArrowButtonComponent(
+      arrowRight,
+      `<i class="fa-solid fa-right-long"></i>`,
+      "arrows-contanier__Rigth button"
+    );
   }
 }
 
